5d202ec9900beeae0c219fc89256d6bb1a0eba5d
Travis <travisdharry@gmail.com>
2021-03-23 13:42:06 -0500

    Adding PyPoll.py file.

---
+++ python_practice.py
@@ -0,0 +1,18 @@
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(counties_dict[county])
+
+
+    # Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+import csv
+dir(csv)
+
+
+dir({'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438})
